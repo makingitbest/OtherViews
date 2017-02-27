7f98b6b7e87cca728d914af05f0a2f2f6066ebb8
wangqiao <makingitbest@163.com>
2017-02-27 10:08:50 +0800

    Initial Commit

---
+++ OtherViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OtherViews
+//
+//  Created by WangQiao on 17/2/27.
+//  Copyright © 2017年 WangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OtherViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OtherViews
+//
+//  Created by WangQiao on 17/2/27.
+//  Copyright © 2017年 WangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
